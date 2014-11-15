1a3af2f71a4230abdc4751a7b942ad820c1137b8
Benjamin Cousin <benou.cousin@gmail.com>
2014-11-15 12:35:23 +0100

    initial commit

---
+++ app/src/main/java/com/codetroopers/androidbootstrap/ui/activity/core/BaseActionBarActivity.java
@@ -0,0 +1,34 @@
+package com.codetroopers.androidbootstrap.ui.activity.core;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.codetroopers.androidbootstrap.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public abstract class BaseActionBarActivity extends ActionBarActivity {
+
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResource());
+        ButterKnife.inject(this);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        }
+    }
+
+    protected abstract int getLayoutResource();
+
+    protected void setActionBarIcon(int iconRes) {
+        toolbar.setNavigationIcon(iconRes);
+    }
+}

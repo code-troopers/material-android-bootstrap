3d12bbcdc8d323859b7a45dcc432543c9afc8fde
Benjamin Cousin <benou.cousin@gmail.com>
2014-11-21 17:48:11 +0100

    add a drawer with a recycler view and ripple effects on items

---
+++ app/src/main/java/com/codetroopers/androidbootstrap/ui/activity/DrawerAdapter.java
@@ -0,0 +1,64 @@
+package com.codetroopers.androidbootstrap.ui.activity;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.codetroopers.androidbootstrap.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class DrawerAdapter extends RecyclerView.Adapter<DrawerAdapter.ViewHolder> {
+    private OnItemClickListener mListener;
+    private int selected;
+
+    public void setActive(int position) {
+        selected = position;
+        notifyDataSetChanged();
+    }
+
+    public interface OnItemClickListener {
+        public void onClick(View view, int position);
+    }
+
+    public DrawerAdapter(OnItemClickListener listener) {
+        this.mListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater vi = LayoutInflater.from(parent.getContext());
+        View v = vi.inflate(R.layout.drawer_list_item, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        viewHolder.name.setText("test" + position);
+        viewHolder.name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onClick(v, position);
+            }
+        });
+        viewHolder.itemView.setActivated(position == selected);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 30;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        @InjectView(android.R.id.text1)
+        TextView name;
+
+        public ViewHolder(View view) {
+            super(view);
+            ButterKnife.inject(this, view);
+        }
+    }
+}
--- app/src/main/java/com/codetroopers/androidbootstrap/ui/activity/HomeActivity.java
+++ app/src/main/java/com/codetroopers/androidbootstrap/ui/activity/HomeActivity.java
@@ -1,38 +1,74 @@
 package com.codetroopers.androidbootstrap.ui.activity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.codetroopers.androidbootstrap.R;
 import com.codetroopers.androidbootstrap.ui.activity.core.BaseActionBarActivity;
 
 import butterknife.InjectView;
 
-public class HomeActivity extends BaseActionBarActivity {
+public class HomeActivity extends BaseActionBarActivity implements DrawerAdapter.OnItemClickListener {
 
     @InjectView(R.id.drawer)
     DrawerLayout drawer;
+    @InjectView(R.id.left_drawer)
+    RecyclerView mDrawerList;
+
     private ActionBarDrawerToggle drawerToggle;
+    private DrawerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // improve performance by indicating the list if fixed size.
+        mDrawerList.setHasFixedSize(true);
+        mDrawerList.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new DrawerAdapter(this);
+        mDrawerList.setAdapter(adapter);
+
         drawer.setDrawerShadow(R.drawable.drawer_shadow, Gravity.START);
         drawer.setStatusBarBackground(R.color.statusBarTransparentColor);
 
-        drawerToggle = new ActionBarDrawerToggle(this, drawer, getToolbar(), R.string.open, R.string.close);
+        drawerToggle = new ActionBarDrawerToggle(this, drawer, getToolbar(), R.string.drawer_open, R.string.drawer_close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
         drawer.setDrawerListener(drawerToggle);
+
+        if (savedInstanceState == null) {
+            selectItem(0);
+        }
     }
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
     }
 
     @Override
@@ -48,7 +84,18 @@ public class HomeActivity extends BaseActionBarActivity {
     }
 
     @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // If the nav drawer is open, hide action items related to the content view
+        boolean drawerOpen = drawer.isDrawerOpen(mDrawerList);
+        menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
         switch (item.getItemId()) {
             case android.R.id.home:
                 drawer.openDrawer(Gravity.START);
@@ -58,4 +105,16 @@ public class HomeActivity extends BaseActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onClick(View view, int position) {
+        selectItem(position);
+    }
+
+    private void selectItem(int position) {
+        drawer.closeDrawer(mDrawerList);
+        adapter.setActive(position);
+
+    }
+
 }

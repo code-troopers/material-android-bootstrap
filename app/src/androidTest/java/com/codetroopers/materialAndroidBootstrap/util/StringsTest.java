3f88b7888ad72c503e967a42cac450404611bfab
Joris Potier <joris.potier@gmail.com>
2015-10-28 10:13:05 +0100

    add junit test which can be run on a jvm (later)

---
+++ app/src/androidTest/java/com/codetroopers/materialAndroidBootstrap/util/StringsTest.java
@@ -0,0 +1,16 @@
+package com.codetroopers.materialAndroidBootstrap.util;
+
+import junit.framework.TestCase;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class StringsTest extends TestCase {
+
+    public void testJoinAnd() throws Exception {
+        assertThat(Strings.joinAnd(", ", " and ", Arrays.asList("England", "France", "Germany")))
+                .isEqualTo("England, France and Germany");
+    }
+}

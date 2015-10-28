ff00e07b29f2ac8d2b208c556adfe4935a98873b
Joris Potier <joris.potier@gmail.com>
2015-10-28 10:16:43 +0100

    tests on jvm ok

--- app/src/test/java/com/codetroopers/materialAndroidBootstrap/util/StringsTest.java
+++ app/src/test/java/com/codetroopers/materialAndroidBootstrap/util/StringsTest.java
@@ -6,9 +6,7 @@ import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
 public class StringsTest extends TestCase {
-
     public void testJoinAnd() throws Exception {
         assertThat(Strings.joinAnd(", ", " and ", Arrays.asList("England", "France", "Germany")))
                 .isEqualTo("England, France and Germany");

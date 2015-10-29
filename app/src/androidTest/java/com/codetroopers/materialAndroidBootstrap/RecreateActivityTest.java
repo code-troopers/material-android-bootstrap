2c1cc252ac112df43f375b9ffe81541894633fb9
Joris Potier <joris.potier@gmail.com>
2015-10-29 13:46:26 +0100

    fix recreate test : it is better to wait for the receation task to complete before the test ending

--- app/src/androidTest/java/com/codetroopers/materialAndroidBootstrap/RecreateActivityTest.java
+++ app/src/androidTest/java/com/codetroopers/materialAndroidBootstrap/RecreateActivityTest.java
@@ -2,6 +2,7 @@ package com.codetroopers.materialAndroidBootstrap;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.Instrumentation;
 import android.os.Build;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
@@ -21,13 +22,15 @@ public abstract class RecreateActivityTest<T extends Activity> {
     public void testRecreate() {
         launchActivity();
 
-        InstrumentationRegistry.getInstrumentation().runOnMainSync(new Runnable() {
+        final Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
+        instrumentation.runOnMainSync(new Runnable() {
             @TargetApi(Build.VERSION_CODES.HONEYCOMB)
             @Override
             public void run() {
                 rule.getActivity().recreate();
             }
         });
+        instrumentation.waitForIdleSync();
     }
 
     /**

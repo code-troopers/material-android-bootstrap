c0e62dde721fb5f3a6b87551aa1124c9bb5cbea5
Joris Potier <joris.potier@gmail.com>
2016-01-27 13:50:09 +0100

    fix back button when the drawer is closed.

--- app/src/main/java/com/codetroopers/materialAndroidBootstrap/ui/activity/HomeActivity.java
+++ app/src/main/java/com/codetroopers/materialAndroidBootstrap/ui/activity/HomeActivity.java
@@ -202,7 +202,7 @@ public class HomeActivity extends BaseActionBarActivity implements
         if (mDrawer.isDrawerOpen(mNavigationView)) {
             mDrawer.closeDrawers();
         } else {
-            //Do fragement replacement or close activity/app
+            finish();
         }
     }
 }

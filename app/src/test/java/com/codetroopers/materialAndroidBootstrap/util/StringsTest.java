d3df9b0f8163dd069a26bf21c9438bccbc286895
Joris Potier <joris.potier@gmail.com>
2016-02-02 21:34:13 +0100

    travis : connectedAndroidTest

--- app/src/test/java/com/codetroopers/materialAndroidBootstrap/util/StringsTest.java
+++ app/src/test/java/com/codetroopers/materialAndroidBootstrap/util/StringsTest.java
@@ -5,13 +5,11 @@ import org.junit.Test;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.fail;
 
 public class StringsTest {
     @Test
     public void testJoinAnd() throws Exception {
         assertThat(Strings.joinAnd(", ", " and ", Arrays.asList("England", "France", "Germany")))
                 .isEqualTo("England, France and Germany");
-        fail("DEBUG");
     }
 }

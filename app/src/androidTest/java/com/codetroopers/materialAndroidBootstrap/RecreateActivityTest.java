aa80b8ca2dd774ecacc2d0d740e99645c21ab4fb
Joris Potier <joris.potier@gmail.com>
2016-02-02 22:59:26 +0100

    travis : attempt to fix tests programatically (tmp solution)

--- app/src/androidTest/java/com/codetroopers/materialAndroidBootstrap/RecreateActivityTest.java
+++ app/src/androidTest/java/com/codetroopers/materialAndroidBootstrap/RecreateActivityTest.java
@@ -1,9 +1,12 @@
 package com.codetroopers.materialAndroidBootstrap;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.Instrumentation;
+import android.os.Build;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
+import android.view.WindowManager;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -20,7 +23,7 @@ public abstract class RecreateActivityTest<T extends Activity> {
     public void testRecreate() {
         launchActivity();
 
-        final Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
+        final Instrumentation instrumentation = getInstrumentation();
         instrumentation.runOnMainSync(() -> rule.getActivity().recreate());
         instrumentation.waitForIdleSync();
     }
@@ -28,7 +31,13 @@ public abstract class RecreateActivityTest<T extends Activity> {
     /**
      * To override when a specific intent has to be given to the activity
      */
+    @TargetApi(Build.VERSION_CODES.ECLAIR)
     protected void launchActivity() {
         rule.launchActivity(null);
+        getInstrumentation().runOnMainSync(() -> rule.getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD));
+    }
+
+    protected Instrumentation getInstrumentation() {
+        return InstrumentationRegistry.getInstrumentation();
     }
 }

5c39d61797b93a06e7d4205647a446423cdfdea8
Joris Potier <joris.potier@gmail.com>
2016-02-05 21:28:25 +0100

    travis : remove attempt to resolve the tests programmatically

--- app/src/androidTest/java/com/codetroopers/materialAndroidBootstrap/RecreateActivityTest.java
+++ app/src/androidTest/java/com/codetroopers/materialAndroidBootstrap/RecreateActivityTest.java
@@ -1,12 +1,9 @@
 package com.codetroopers.materialAndroidBootstrap;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.Instrumentation;
-import android.os.Build;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
-import android.view.WindowManager;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -23,7 +20,7 @@ public abstract class RecreateActivityTest<T extends Activity> {
     public void testRecreate() {
         launchActivity();
 
-        final Instrumentation instrumentation = getInstrumentation();
+        final Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
         instrumentation.runOnMainSync(() -> rule.getActivity().recreate());
         instrumentation.waitForIdleSync();
     }
@@ -31,13 +28,7 @@ public abstract class RecreateActivityTest<T extends Activity> {
     /**
      * To override when a specific intent has to be given to the activity
      */
-    @TargetApi(Build.VERSION_CODES.ECLAIR)
     protected void launchActivity() {
         rule.launchActivity(null);
-        getInstrumentation().runOnMainSync(() -> rule.getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD));
-    }
-
-    protected Instrumentation getInstrumentation() {
-        return InstrumentationRegistry.getInstrumentation();
     }
 }
